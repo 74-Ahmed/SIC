768868e0dbf25e35078f04a963bb92c689f63481
74-Ahmed <68439414+74-Ahmed@users.noreply.github.com>
2021-08-11 22:15:54 +0200

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,49 @@
+for row  in range (7) :
+        for col in range (5) :
+            if ((col==0 or col==4) and row !=0) or ((row==0 or row==3) and (col>0 and col<4)):
+                print ("*",end="")
+            else:
+                print (end=" ")
+        print()
+
+print("\n")
+
+for row in range(7):
+        for col in range(5):
+            if ((col == 0 or col == 4)  or (row == 3) and (col > 0 and col < 4)):
+                    print("*", end="")
+            else:
+                    print(end=" ")
+        print()
+
+print("\n")
+
+for row in range(7):
+        for col in range(7):
+            if ((col == 0 or col == 6)  or (row == col) and (col > 0 and col < 4)) or (row==1 and col==5 ) or (row==2 and col==4):
+                    print("*", end="")
+            else:
+                    print(end=" ")
+        print()
+
+print("\n")
+
+for row in range(7):
+        for col in range(5):
+            if (col == 0 or  (row == 0 or row==3 or row==6) and (col > 0)):
+                    print("*", end="")
+            else:
+                    print(end=" ")
+        print()
+
+print("\n")
+
+for row in range(7):
+        for col in range(5):
+            if (col == 0) or  (col==4 and (row!=0 and row!=6)) or (row == 0 or row==6)  and (col > 0 and col<4):
+                    print("*", end="")
+            else:
+                    print(end=" ")
+        print()
+
+print("\n")
